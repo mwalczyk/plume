1944a25c84a910ef4d7eda2756ab767199cfe6f0
mwalczyk <mwalczyk2@gmail.com>
2018-01-28 22:53:22 -0800

    Fixes issue with namespaces in RenderPassBuilder code.

---
+++ build/CMakeFiles/plume_app.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/plume_app.dir/src/vk/misc/Geometry.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/misc/Utils.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/misc/ResourceManager.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/Pipeline.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/RenderPass.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/DescriptorPool.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/Device.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/Sampler.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/Swapchain.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/Buffer.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/CommandPool.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/Image.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/Instance.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/CommandBuffer.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/Window.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/Synchronization.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/ShaderModule.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/DeviceMemory.cpp.o"
+  "CMakeFiles/plume_app.dir/src/vk/wrappers/Framebuffer.cpp.o"
+  "CMakeFiles/plume_app.dir/deps/spirv-cross/spirv_msl.cpp.o"
+  "CMakeFiles/plume_app.dir/deps/spirv-cross/spirv_cfg.cpp.o"
+  "CMakeFiles/plume_app.dir/deps/spirv-cross/spirv_glsl.cpp.o"
+  "CMakeFiles/plume_app.dir/deps/spirv-cross/main.cpp.o"
+  "CMakeFiles/plume_app.dir/deps/spirv-cross/samples/cpp/atomics.cpp.o"
+  "CMakeFiles/plume_app.dir/deps/spirv-cross/samples/cpp/multiply.cpp.o"
+  "CMakeFiles/plume_app.dir/deps/spirv-cross/samples/cpp/shared.cpp.o"
+  "CMakeFiles/plume_app.dir/deps/spirv-cross/spirv_cpp.cpp.o"
+  "CMakeFiles/plume_app.dir/deps/spirv-cross/spirv_cross.cpp.o"
+  "CMakeFiles/plume_app.dir/deps/spirv-cross/spirv_hlsl.cpp.o"
+  "plume_app.pdb"
+  "plume_app"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/plume_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
