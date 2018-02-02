fc7201be3047023a2f49be85543a9d404a20853e
mwalczyk <mwalczyk2@gmail.com>
2018-02-01 19:05:43 -0800

    Finishes initial CMake integration.

--- checkout_and_build_deps.sh
+++ checkout_and_build_deps.sh
@@ -9,10 +9,12 @@ git clone https://github.com/google/glslang.git
 git clone https://github.com/KhronosGroup/SPIRV-Tools.git spirv-tools
 git clone https://github.com/KhronosGroup/SPIRV-Headers.git spirv-tools/external/spirv-headers
 
-# build shaderc
-# cd ..
-# mkdir build
-# cd build
-# cmake ..
-# cmake --build . --config Debug
-# cmake --build . --config Release
+# copy header/source files from spirv-cross into new folders that
+# will be included in the build
+cd ../../..
+mkdir include/vk/spirv-cross
+mkdir src/vk/spirv-cross
+mv deps/spirv-cross/*.hpp include/vk/spirv-cross
+mv deps/spirv-cross/*.cpp src/vk/spirv-cross
+rm src/vk/spirv-cross/main.cpp
+
